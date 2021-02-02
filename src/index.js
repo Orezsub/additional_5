24441e49217ba24d2aed880fccd659dcf06c3c25
Orezsub <orezsub3345@gmail.com>
2021-02-02 11:24:45 +0300

    working version of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const openedBrackets = [];
+    const closedBrackets = [];
+    bracketsConfig.forEach(bracket => {
+        openedBrackets.push(bracket[0])
+        closedBrackets.push(bracket[1])
+    })
+    console.log(openedBrackets, closedBrackets)
+    console.log(str.split(''))
+    const stack = [];
+    let res = true
+    str.split('').forEach(bracket => {
+        if (openedBrackets.includes(bracket)) {
+            if (closedBrackets.includes(bracket) && bracket === stack[stack.length - 1]) {
+                stack.pop()
+            }
+            else {
+                stack.push(bracket)
+            }
+        }
+        else if (openedBrackets.indexOf(stack.pop()) !== closedBrackets.indexOf(bracket)) {
+            res = false;
+        }
+    })
+    console.log(stack.length, stack)
+    return res && stack.length === 0
 }
